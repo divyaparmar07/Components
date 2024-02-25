827b4fe35b7b9384a9c1e661ef8c59d06027176a
Divya Parmar <divyaparmar0207@gmail.com>
2024-02-25 22:03:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/components/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.components;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        ConstraintLayout l1 = findViewById(R.id.custom1);
+//
+//        Button btnCustom = l1.findViewById(R.id.customButton);
+//        btnCustom.setText("Custom Button1");
+//        btnCustom.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(),"Custom Button1 Clicked!",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        l1 = findViewById(R.id.custom2);
+//        Button btnCustom2 = l1.findViewById(R.id.customButton);
+//        btnCustom2.setText("Custom Button2");
+//        btnCustom2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getApplicationContext(),"Custom Button2 Clicked!",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+    }
+}
